caf3cb6b3b5bdde41fcd15e9e6af2216b5752a24
yasu-git <yasu.git.33@gmail.com>
2024-12-02 00:51:08 +0900

    mongoose connectDB

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//database.jsを追加
+const connectDB = require('./config/database');
 
 var app = express();
+
+// MongoDBに接続
+connectDB();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
